7614059d693587824574ab948cafa2b169d17832
ilanoprix <82501303+ilanoprix@users.noreply.github.com>
2021-04-14 16:21:28 +0800

    Add files via upload

---
+++ Personal Information.py
@@ -0,0 +1,14 @@
+print("Laboratory Activity #4")
+print("Coded by: Prixther Jay S. Ilano, BIT 21")
+print("")
+
+Age = 20
+Address = "Malagasang 2C, Imus, Cavite\n"
+FirstName = "Prixther Jay"
+MiddleInitial = "S."
+LastName = "Ilano"
+Space = " "
+Comma = ","
+
+
+print(FirstName + Space + MiddleInitial + Space + LastName + Comma + Space + str(Age) + Comma + Space + Address)
